d701a610e6b7b604b24fa585155fb1365cb681fd
pedro <pedro@Linux-Mint>
2024-01-26 11:26:05 -0300

    Aula 23: COnversão Número - String

---
+++ exercicios/src/fundamentos/ConversaoNumeroString.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class ConversaoNumeroString {
+
+	public static void main(String[] args) {
+		
+		// Para converter um numero para string, é necessário utlizar o seu respectiva Wrapper
+		
+		Integer num1 = 10000;
+		System.out.println(num1.toString().length());
+		
+		int num2 = 100000;
+		System.out.println(Integer.toString(num2).length());
+		
+		System.out.println(("" + num1).length());
+		System.out.println(("" + num2).length());
+		
+	}
+	
+}
