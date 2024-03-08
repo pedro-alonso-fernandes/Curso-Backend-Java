c73582229dd22bee9516348a8e251b24f45aac5b
pedro <pedro@Linux-Mint>
2024-03-08 17:59:46 -0300

    Cap 4, Aula 10: For #01

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,27 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		
+		for(int contador = 0; contador <= 20; contador += 2) {
+			System.out.println("i = " + contador);
+		}
+		
+		/* Laço infinito!!!
+		
+		for(;;){
+			System.out.println("Esse programa nunca vai parar!");
+		}
+		
+		ou 
+		
+		for(;true;){
+			System.out.println("Esse programa nunca vai parar!");
+		}
+		
+		*/
+		
+	}
+	
+}
