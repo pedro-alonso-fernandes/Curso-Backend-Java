e6d5d9ea7952fc9d11725deacec2f444cf97034b
pedro <pedro@Linux-Mint>
2024-03-09 11:49:50 -0300

    Cap 4, Aula 17: Desafio For

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+		
+		String valor = "#";
+		for(String i = ""; !i.equals("aaaaa"); i += "a") {
+			System.out.println(valor);
+			valor += "#";
+		}
+		
+	}
+	
+}
