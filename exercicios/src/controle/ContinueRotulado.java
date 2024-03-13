5c05231d59a13e921986f195d5a6c023dad95e19
pedro <pedro@Linux-Mint>
2024-03-13 18:30:51 -0300

    Cap 4, Aula 24: Continue Rotulado

---
+++ exercicios/src/controle/ContinueRotulado.java
@@ -0,0 +1,25 @@
+package controle;
+
+public class ContinueRotulado {
+
+	public static void main(String[] args) {
+
+		// "externo" é um rótulo que, no caso, nomea o primeiro "for".
+		// Pode-se escolher o nome que quiser (assim como uma variável).
+		// Com o "for" nomeado, é possível quebrar sua repetição de qualquer parte
+		// do código, utilizando o "continue":
+		externo: for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (i == 1) {
+					continue externo;
+				}
+				System.out.printf("[%d %d] ", i, j);
+			}
+			System.out.println();
+		}
+
+		System.out.println("Fim!");
+
+	}
+
+}
