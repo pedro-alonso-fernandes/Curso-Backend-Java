2c1be04f398f0114e99441a8fee7dbba685142f5
pedro <pedro@Linux-Mint>
2024-03-18 22:02:46 -0300

    Cap 5, Aula 11: Desafio Método Data

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,4 +6,57 @@ public class Data {
 	String mes;
 	int ano;
 	
+	String dataFormatada() {
+		String data = "";
+		
+		if(mes.equalsIgnoreCase("janeiro")) {
+			mes = "01";
+		} else if(mes.equalsIgnoreCase("fevereiro")){
+			mes = "02";
+		} else if(mes.equalsIgnoreCase("março") || mes.equalsIgnoreCase("marco")){
+			mes = "03";
+		} else if(mes.equalsIgnoreCase("abril")){
+			mes = "04";
+		} else if(mes.equalsIgnoreCase("maio")){
+			mes = "05";
+		} else if(mes.equalsIgnoreCase("junho")){
+			mes = "06";
+		} else if(mes.equalsIgnoreCase("julho")){
+			mes = "07";
+		} else if(mes.equalsIgnoreCase("agosto")){
+			mes = "08";
+		} else if(mes.equalsIgnoreCase("setembro")){
+			mes = "09";
+		} else if(mes.equalsIgnoreCase("outubro")){
+			mes = "10";
+		} else if(mes.equalsIgnoreCase("novembro")){
+			mes = "11";
+		} else if(mes.equalsIgnoreCase("dezembro")){
+			mes = "12";
+		} else if(mes.equals("01") || mes.equals("02") || mes.equals("03") || mes.equals("04") || mes.equals("05") || mes.equals("06") || mes.equals("07") || mes.equals("08") || mes.equals("09") || mes.equals("10") || mes.equals("11") || mes.equals("12")) {
+			
+		} else if(mes.equals("1") || mes.equals("2") || mes.equals("3") || mes.equals("4") || mes.equals("5") || mes.equals("6") || mes.equals("7") || mes.equals("8") || mes.equals("9")) {
+			mes = "0" + mes;
+		} else {
+			mes = "erro";
+		}
+		
+		switch (mes) {
+		case "erro":
+			data = "\nErro! mês inválido!";
+			break;
+		default:
+			// A função "format" da "String", retorna uma String formatada de acordo
+			// com as características do "printf":
+			data = String.format("%d/%s/%d", dia, mes, ano);
+			break;
+		}
+		
+		return data;
+	}
+	
+	void imprimirDataFormatada() {
+		System.out.println(dataFormatada());
+	}
+	
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -12,48 +12,17 @@ public class DataTeste {
 		System.out.print("Dia: ");
 		data.dia = entrada.nextInt();
 		
-		System.out.print("Nome do mês: ");
+		System.out.print("Mês: ");
 		data.mes = entrada.next();
 		
 		System.out.print("Ano: ");
 		data.ano = entrada.nextInt();
 		
-		if(data.mes.equalsIgnoreCase("janeiro")) {
-			data.mes = "01";
-		} else if(data.mes.equalsIgnoreCase("fevereiro")){
-			data.mes = "02";
-		} else if(data.mes.equalsIgnoreCase("março") || data.mes.equalsIgnoreCase("marco")){
-			data.mes = "03";
-		} else if(data.mes.equalsIgnoreCase("abril")){
-			data.mes = "04";
-		} else if(data.mes.equalsIgnoreCase("maio")){
-			data.mes = "05";
-		} else if(data.mes.equalsIgnoreCase("junho")){
-			data.mes = "06";
-		} else if(data.mes.equalsIgnoreCase("julho")){
-			data.mes = "07";
-		} else if(data.mes.equalsIgnoreCase("agosto")){
-			data.mes = "08";
-		} else if(data.mes.equalsIgnoreCase("setembro")){
-			data.mes = "09";
-		} else if(data.mes.equalsIgnoreCase("outubro")){
-			data.mes = "10";
-		} else if(data.mes.equalsIgnoreCase("novembro")){
-			data.mes = "11";
-		} else if(data.mes.equalsIgnoreCase("dezembro")){
-			data.mes = "12";
-		} else {
-			data.mes = "erro";
-		}
+		String dataFinal = data.dataFormatada();
 		
-		switch (data.mes) {
-		case "erro":
-			System.out.println("O mês informado é inválido");
-			break;
-		default: 
-			System.out.printf("A data informada é: %d/%s/%d", data.dia, data.mes, data.ano);
-			break;
-		}
+		System.out.println("A data informada é: " + dataFinal);
+		
+		data.imprimirDataFormatada();
 		
 		entrada.close();
 		
