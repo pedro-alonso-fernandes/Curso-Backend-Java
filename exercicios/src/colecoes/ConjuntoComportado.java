858718276d9f82670584281be6b33ca80625aaca
pedro-alonso-fernandes <pedroalonso2005@hotmail.com>
2024-06-01 21:51:58 -0300

    Cap 6, Aula 12: Set #02

---
+++ exercicios/src/colecoes/ConjuntoComportado.java
@@ -0,0 +1,35 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+
+	public static void main(String[] args) {
+		
+		// Para fazer com que um conjunto aceite apenas um tipo de valor, deve-se
+		// colocar o tipo desejado entre os sinais de maior e menor que(generics):
+		Set<String> lista = new HashSet<>();
+		lista.add("Pedro");
+		lista.add("Gustavo");
+		lista.add("Thalles");
+		lista.add("Vallix");
+		
+		for(String nome: lista) {
+			System.out.println(nome);
+		}
+		
+		// O "SortedSet" também é um conjunto do tipo "set", porém ele ordena os valores do conjunto:
+		SortedSet<Integer> nums = new TreeSet<>();
+		nums.add(4);
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		for(int n: nums) {
+			System.out.println(n);
+		}
+	}
+}
