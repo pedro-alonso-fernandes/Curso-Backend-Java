7ab441cc84b18f887de19f9bb4216a2e069243b1
pedro-alonso-fernandes <pedroalonso2005@hotmail.com>
2024-06-13 23:04:53 -0300

    Cap 7, Aula 14: Enum

---
+++ exercicios/src/oo/heranca/Direcao.java
@@ -0,0 +1,13 @@
+package oo.heranca;
+
+public enum Direcao {
+
+	// O "Enum" serve para padronizar valores constantes, ou seja, valores em que já se sabe
+	// sua quantidade exata e por isso não vão mudar (como por exemplo: dias da semana, meses
+	// do ano, etc).
+	
+	// Nesse caso, usei para definir a direção dos movimentos de um jogo 2D.
+	
+	NORTE, LESTE, SUL, OESTE;
+	
+}
