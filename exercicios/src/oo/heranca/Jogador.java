afb131c3a1a01c8bb263849b1ecf8ae30bedd3e9
pedro-alonso-fernandes <pedroalonso2005@hotmail.com>
2024-06-14 23:39:58 -0300

    Cap 7, Aula 15: Herança #01

--- exercicios/src/oo/heranca/Jogador.java
+++ exercicios/src/oo/heranca/Jogador.java
@@ -2,8 +2,30 @@ package oo.heranca;
 
 public class Jogador {
 
+	int vida = 100;
 	int x;
 	int y;
+	
+	boolean atacar(Jogador oponente) {
+		
+		// O método "Math.abs()" deixa um número em módulo, ou seja, ele sempre
+		// retornará um número positivo.
+		int deltaX = Math.abs(x - oponente.x);
+		int deltaY = Math.abs(y - oponente.y);
+		
+		if(deltaX == 0 && deltaY == 1) {
+			oponente.vida -= 10;
+			return true;
+		}
+		else if(deltaX == 1 && deltaY == 0) {
+			oponente.vida -= 10;
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
 	
 	boolean andar(Direcao direcao) {
 		
