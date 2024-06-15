7620aed09d49fe34e29fd24d3f2e18ffac300ec5
pedro-alonso-fernandes <pedroalonso2005@hotmail.com>
2024-06-15 00:02:02 -0300

    Cap 7, Aula 16: Herança #02

--- exercicios/src/oo/heranca/Heroi.java
+++ exercicios/src/oo/heranca/Heroi.java
@@ -2,4 +2,15 @@ package oo.heranca;
 
 public class Heroi extends Jogador {
 
+	// Reescrevi o método "atacar" do Herói para que ele tenha um ataque mais
+	// forte do que o Monstro (no caso, o ataque do herói é 3x mais forte):
+	boolean atacar(Jogador oponente) {
+		// O comando "super" se refere a classe mãe da classe atual. No caso, 
+		// ele está se referindo a classe "Jogador".
+		boolean atq1 = super.atacar(oponente);
+		boolean atq2 = super.atacar(oponente);
+		boolean atq3 = super.atacar(oponente);
+		
+		return atq1 || atq2 || atq3;
+	}
 }
