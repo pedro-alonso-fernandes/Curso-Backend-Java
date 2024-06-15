c299f834ff3f01643b70e610eba2b3444ef83659
pedro-alonso-fernandes <pedroalonso2005@hotmail.com>
2024-06-15 17:20:11 -0300

    Complemento do desafio

--- exercicios/src/oo/heranca/desafio/Camaro.java
+++ exercicios/src/oo/heranca/desafio/Camaro.java
@@ -2,6 +2,9 @@ package oo.heranca.desafio;
 
 public class Camaro extends Carro {
 
+	// O "@Override" mostra ao eclipse que você está sobreescrevendo um
+	// método já existente na classe pai.
+	@Override
 	void acelerar() {
 		for(int i = 0; i < 3; i++) {
 			super.acelerar();
