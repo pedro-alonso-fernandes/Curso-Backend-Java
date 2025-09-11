e52598d7b834349bce655e7e75f783021743553a
Pedro Alonso Fernandes <pedroalonso2005@hotmail.com>
2025-09-11 12:27:47 -0300

    Cap 8, Aula 3: Interfaces Funcionais #01

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -1,5 +1,11 @@
 package lambdas;
 
+// Uma interface funcional é uma interface que possui apenas um método.
+@FunctionalInterface 
+
+// Obs: As anotações servem para adicionar validações no código. Ex:
+// Se eu adicionar mais um método nessa interface, vai dar erro já que
+// ela tem a anotação de uma interface funcional.
 public interface Calculo {
 	
 	double executar(double a, double b);
