518e4a966bfb564df798d48ab3afde3edc177694
Pedro Alonso Fernandes <pedroalonso2005@hotmail.com>
2025-09-23 08:32:23 -0300

    Cap 8, Aula 7: Foreach Funcional #02

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -18,7 +18,19 @@ public class Foreach {
 		aprovados.forEach((nome) -> { System.out.println(nome + "!!!"); });
 		
 		// Usando method refence para imprimir os nomes
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nMethod Reference #01...");
 		aprovados.forEach(System.out::println);
+		
+		// Usando lambda para definir o método foreach e imprimir usando método statico
+		System.out.println("\nLambda #02...");
+		aprovados.forEach(nome -> meuImprimir(nome));
+		
+		// Usando method reference e método statico para imprimir
+		System.out.println("\nMethod Reference #02...");
+		aprovados.forEach(Foreach::meuImprimir);
+	}
+	
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é " + nome);
 	}
 }
